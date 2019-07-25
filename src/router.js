8de534c9243eaac839140cb0f62b90a7004866c6
Patrick Lee <patrick@Patrickui-MacBook-Pro.local>
2019-07-25 11:51:27 +0900

    first commit

--- src/router.js
+++ src/router.js
@@ -4,22 +4,36 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+  }
+]
+
+const router = new Router({
+  mode: 'hash', // history or hash
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+  routes
 })
+
+// 페이지 이동 직전에 호출
+router.beforeEach((to, from, next) => {
+  next()
+})
+
+// 페이지 이동 이후에 호출
+router.afterEach(() => {
+
+})
+
+export default router
